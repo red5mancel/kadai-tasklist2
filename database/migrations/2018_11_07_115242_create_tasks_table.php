67c641d0d6f5d9d39a7c947100d0e8eff344c7e6
takaharu.ochiai <chibichanmgo@yahoo.co.jp>
2018-11-07 04:44:49 +0000

    add column2

--- database/migrations/2018_11_07_115242_create_tasks_table.php
+++ database/migrations/2018_11_07_115242_create_tasks_table.php
@@ -15,7 +15,9 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('content');
+            $table->string('content'); 
+            $table->string('title');
+            $table->string('status', 10);
             $table->timestamps();
         });
     }
